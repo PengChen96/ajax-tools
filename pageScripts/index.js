0153e92675f7ddc1ef2eb8a0978e72fe54dae941
pc <2601349541@qq.com>
2022-07-05 20:41:11 +0800

    feat: 支持 fetch

--- pageScripts/index.js
+++ pageScripts/index.js
@@ -1,153 +1,5 @@
-// 命名空间
-let ajax_interceptor_qoweifjqon = {
-  settings: {
-    ajaxInterceptor_switchOn: false,
-    ajaxInterceptor_rules: [],
-  },
-  originalXHR: window.XMLHttpRequest,
-  myXHR: function () {
-    let pageScriptEventDispatched = false;
-    const modifyResponse = () => {
-      ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_rules.forEach(({filterType = 'normal', switchOn = true, match, overrideTxt = ''}) => {
-        let matched = false;
-        if (switchOn && match) {
-          if (filterType === 'normal' && this.responseURL.indexOf(match) > -1) {
-            matched = true;
-          } else if (filterType === 'regex' && this.responseURL.match(new RegExp(match, 'i'))) {
-            matched = true;
-          }
-        }
-        if (matched) {
-          this.responseText = overrideTxt;
-          this.response = overrideTxt;
 
-          if (!pageScriptEventDispatched) {
-            window.dispatchEvent(new CustomEvent("pageScript", {
-              detail: {url: this.responseURL, match}
-            }));
-            pageScriptEventDispatched = true;
-          }
-        }
-      })
-    }
-
-    const xhr = new ajax_interceptor_qoweifjqon.originalXHR;
-    for (let attr in xhr) {
-      if (attr === 'onreadystatechange') {
-        xhr.onreadystatechange = (...args) => {
-          if (this.readyState == 4) {
-            // 请求成功
-            if (ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_switchOn) {
-              // 开启拦截
-              modifyResponse();
-            }
-          }
-          this.onreadystatechange && this.onreadystatechange.apply(this, args);
-        }
-        continue;
-      } else if (attr === 'onload') {
-        xhr.onload = (...args) => {
-          // 请求成功
-          if (ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_switchOn) {
-            // 开启拦截
-            modifyResponse();
-          }
-          this.onload && this.onload.apply(this, args);
-        }
-        continue;
-      }
-
-      if (typeof xhr[attr] === 'function') {
-        this[attr] = xhr[attr].bind(xhr);
-      } else {
-        // responseText和response不是writeable的，但拦截时需要修改它，所以修改就存储在this[`_${attr}`]上
-        if (attr === 'responseText' || attr === 'response') {
-          Object.defineProperty(this, attr, {
-            get: () => this[`_${attr}`] == undefined ? xhr[attr] : this[`_${attr}`],
-            set: (val) => this[`_${attr}`] = val,
-            enumerable: true
-          });
-        } else {
-          Object.defineProperty(this, attr, {
-            get: () => xhr[attr],
-            set: (val) => xhr[attr] = val,
-            enumerable: true
-          });
-        }
-      }
-    }
-  },
-
-  originalFetch: window.fetch.bind(window),
-  myFetch: function (...args) {
-    return ajax_interceptor_qoweifjqon.originalFetch(...args).then((response) => {
-      let txt = undefined;
-      ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_rules.forEach(({filterType = 'normal', switchOn = true, match, overrideTxt = ''}) => {
-        let matched = false;
-        if (switchOn && match) {
-          if (filterType === 'normal' && response.url.indexOf(match) > -1) {
-            matched = true;
-          } else if (filterType === 'regex' && response.url.match(new RegExp(match, 'i'))) {
-            matched = true;
-          }
-        }
-
-        if (matched) {
-          window.dispatchEvent(new CustomEvent("pageScript", {
-            detail: {url: response.url, match}
-          }));
-          txt = overrideTxt;
-        }
-      });
-
-      if (txt !== undefined) {
-        const stream = new ReadableStream({
-          start(controller) {
-            // const bufView = new Uint8Array(new ArrayBuffer(txt.length));
-            // for (var i = 0; i < txt.length; i++) {
-            //   bufView[i] = txt.charCodeAt(i);
-            // }
-            controller.enqueue(new TextEncoder().encode(txt));
-            controller.close();
-          }
-        });
-
-        const newResponse = new Response(stream, {
-          headers: response.headers,
-          status: response.status,
-          statusText: response.statusText,
-        });
-        const proxy = new Proxy(newResponse, {
-          get: function (target, name) {
-            switch (name) {
-              case 'ok':
-              case 'redirected':
-              case 'type':
-              case 'url':
-              case 'useFinalURL':
-              case 'body':
-              case 'bodyUsed':
-                return response[name];
-            }
-            return target[name];
-          }
-        });
-
-        for (let key in proxy) {
-          if (typeof proxy[key] === 'function') {
-            proxy[key] = proxy[key].bind(newResponse);
-          }
-        }
-
-        return proxy;
-      } else {
-        return response;
-      }
-    });
-  },
-}
-
-let ajax_tools_space = {
+const ajax_tools_space = {
   ajaxToolsSwitchOn: true,
   ajaxDataList: [],
   originalXHR: window.XMLHttpRequest,
@@ -167,7 +19,7 @@ let ajax_tools_space = {
             this.responseText = responseText;
             this.response = responseText;
             // console.info('ⓢ ►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►► ⓢ');
-            console.groupCollapsed(`%c匹配路径/规则：${request}`, 'background-color: #108ee9; color: white; padding: 4px');
+            console.groupCollapsed(`%c XHR匹配路径/规则：${request}`, 'background-color: #108ee9; color: white; padding: 4px');
             console.info(`%c接口路径：`, 'background-color: #ff8040; color: white;', this.responseURL);
             console.info('%c返回出参：', 'background-color: #ff5500; color: white;', JSON.parse(responseText));
             console.groupEnd();
@@ -221,6 +73,67 @@ let ajax_tools_space = {
       }
 
     }
+  },
+  originalFetch: window.fetch.bind(window),
+  myFetch: function (...args) {
+    return ajax_tools_space.originalFetch(...args).then((response) => {
+      let text = undefined;
+      const interfaceList = [];
+      ajax_tools_space.ajaxDataList.forEach((item) => {
+        interfaceList.push(...(item.interfaceList || []));
+      });
+      interfaceList.forEach(({open = true, request, responseText}) => {
+        if (open) {
+          let matched = false;
+          if (request && response.url.includes(request)) {
+            matched = true;
+          }
+          if (matched && responseText) {
+            text = responseText;
+            // console.info('ⓢ ►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►► ⓢ');
+            console.groupCollapsed(`%c Fetch匹配路径/规则：${request}`, 'background-color: #108ee9; color: white; padding: 4px');
+            console.info(`%c接口路径：`, 'background-color: #ff8040; color: white;', response.url);
+            console.info('%c返回出参：', 'background-color: #ff5500; color: white;', JSON.parse(responseText));
+            console.groupEnd();
+            // console.info('ⓔ ▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣ ⓔ')
+          }
+        }
+      });
+      if (text !== undefined) {
+        const stream = new ReadableStream({
+          start(controller) {
+            controller.enqueue(new TextEncoder().encode(text));
+            controller.close();
+          }
+        });
+        const newResponse = new Response(stream, {
+          headers: response.headers,
+          status: response.status,
+          statusText: response.statusText,
+        });
+        const responseProxy = new Proxy(newResponse, {
+          get: function (target, name) {
+            switch (name) {
+              case 'body':
+              case 'bodyUsed':
+              case 'ok':
+              case 'redirected':
+              case 'type':
+              case 'url':
+                return response[name];
+            }
+            return target[name];
+          }
+        });
+        for (let key in responseProxy) {
+          if (typeof responseProxy[key] === 'function') {
+            responseProxy[key] = responseProxy[key].bind(newResponse);
+          }
+        }
+        return responseProxy;
+      }
+      return response;
+    })
   }
 }
 
@@ -229,11 +142,16 @@ window.addEventListener("message", function (event) {
   if (data.type === 'ajaxTools' && data.to === 'pageScript') {
     console.log('【pageScripts/index.js】', data);
     ajax_tools_space[data.key] = data.value;
+    //
+    if (data.key === 'ajaxToolsSwitchOn') {
+      if (ajax_tools_space.ajaxToolsSwitchOn) {
+        window.XMLHttpRequest = ajax_tools_space.myXHR;
+        window.fetch = ajax_tools_space.myFetch;
+      } else {
+        window.XMLHttpRequest = ajax_tools_space.originalXHR;
+        window.fetch = ajax_tools_space.originalFetch;
+      }
+    }
   }
-  //
-  if (ajax_tools_space.ajaxToolsSwitchOn) {
-    window.XMLHttpRequest = ajax_tools_space.myXHR;
-  } else {
-    window.XMLHttpRequest = ajax_tools_space.originalXHR;
-  }
+
 }, false);
