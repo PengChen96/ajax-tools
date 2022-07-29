7407ee05b23e44c7bea1c998532d60e868e1ebe4
pc <2601349541@qq.com>
2022-07-29 09:51:24 +0800

    feat: 修改缩小的iframe宽度

--- content.js
+++ content.js
@@ -57,7 +57,7 @@ if (window.self === window.top) {
           if (iframeZoom === 'out') { // 缩小
             iframe.style.setProperty('height', '40px', 'important');
             let timer = setTimeout(() => {
-              iframe.style.setProperty('width', '100px', 'important');
+              iframe.style.setProperty('width', '170px', 'important');
               clearTimeout(timer);
             }, 400);
           } else if (iframeZoom === 'in') { // 放大
