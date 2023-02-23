bff6eb1ce75654d3b08882dcdde6e2236d4f596f
pc <2601349541@qq.com>
2023-02-23 19:25:40 +0800

    feat: TS

---
+++ html/iframePage/common/value.ts
@@ -0,0 +1,39 @@
+export interface AjaxDataListObject {
+  summaryText: string,
+  collapseActiveKeys: string [],
+  headerClass: string,
+  interfaceList: DefaultInterfaceObject[]
+}
+export interface DefaultInterfaceObject {
+  key: string,
+  open: boolean,
+  matchType: string, // normal regex
+  matchMethod: string, // GET、POST、PUT、DELETE、HEAD、OPTIONS、CONNECT、TRACE、PATCH
+  request: string,
+  requestDes: string,
+  responseText: string,
+  language: string, // json javascript
+  [key: string]: any;
+}
+
+export const defaultInterface: DefaultInterfaceObject = {
+  key: '1',
+  open: true,
+  matchType: 'normal',
+  matchMethod: '',
+  request: '',
+  requestDes: '',
+  responseText: '',
+  language: 'json',
+};
+
+export const defaultAjaxDataList: AjaxDataListObject[] = [
+  {
+    summaryText: 'Group Name (Editable)',
+    collapseActiveKeys: ['1'],
+    headerClass: 'ajax-tools-color-volcano',
+    interfaceList: [
+      { ...defaultInterface },
+    ]
+  },
+];
