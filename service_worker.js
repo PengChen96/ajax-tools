11b10620b30650d2664755fbbe57057a45e61815
pc <2601349541@qq.com>
2023-02-24 10:57:50 +0800

    feat: Grooming

--- service_worker.js
+++ service_worker.js
@@ -1,36 +1,35 @@
-
-chrome.action.onClicked.addListener(() => {
-
-  chrome.storage.local.get("iframeVisible", ({iframeVisible}) => {
-    chrome.tabs.query(
-      {active: true, currentWindow: true},
-      function (tabs) {
-        // 发送消息到content.js
-        chrome.tabs.sendMessage(
-          tabs[0].id,
-          {type: 'iframeToggle', iframeVisible},
-          function (response) {
-            console.log('【service_worker.js】->【content】【ajax-tools-iframe-show】Return message:', response);
-            chrome.storage.local.set({iframeVisible: response.nextIframeVisible});
-          }
-        );
-      }
-    );
+function sendMessageToContentScript(tabId, message) {
+  return new Promise((resolve) => {
+    chrome.tabs.sendMessage(tabId, message, (response) => {
+      console.log('【service_worker.js】->【content】【ajax-tools-iframe-show】Return message:', response);
+      resolve(response);
+    });
   });
+}
+async function toggleIframeVisibility() {
+  const {iframeVisible} = await chrome.storage.local.get({iframeVisible: true});
+  const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+  const response = await sendMessageToContentScript(tabs[0].id, {type: 'iframeToggle', iframeVisible});
+  await chrome.storage.local.set({iframeVisible: response.nextIframeVisible});
+}
+function setSwitchBadge (switchValue) {
+  chrome.action.setBadgeText({text: switchValue ? 'ON' : 'OFF'});
+  chrome.action.setBadgeBackgroundColor({color: switchValue ? '#4480f7' : '#bfbfbf'});
+}
 
+chrome.action.onClicked.addListener(async () => {
+  await toggleIframeVisibility();
 });
 
 chrome.storage.local.get(['ajaxToolsSwitchOn'], (result) => {
   const {ajaxToolsSwitchOn = true} = result;
-  chrome.action.setBadgeText({text: ajaxToolsSwitchOn ? 'ON' : 'OFF'});
-  chrome.action.setBadgeBackgroundColor({color: ajaxToolsSwitchOn ? '#4480f7' : '#bfbfbf'});
+  setSwitchBadge(ajaxToolsSwitchOn);
 });
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
     if (key === 'ajaxToolsSwitchOn') {
-      chrome.action.setBadgeText({text: newValue ? 'ON' : 'OFF'});
-      chrome.action.setBadgeBackgroundColor({color: newValue ? '#4480f7' : '#bfbfbf'});
+      setSwitchBadge(newValue);
     }
   }
 });
