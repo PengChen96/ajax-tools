d1ab5abee94209873cce89bf73d97288ee2f3f2f
pc <2601349541@qq.com>
2023-04-05 14:48:47 +0800

    feat: optimization logs

--- pageScripts/index.js
+++ pageScripts/index.js
@@ -93,9 +93,9 @@ const ajax_tools_space = {
           this.status = 200;
         }
         // console.info('ⓢ ►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►► ⓢ');
-        console.groupCollapsed(`%c Matched XHR Path/Rule：${matchedInterface.request}`, 'background-color: #108ee9; color: white; padding: 4px');
-        console.info(`%cRequest Url：`, 'background-color: #ff8040; color: white;', this.responseURL);
-        console.info('%cResponse Text：', 'background-color: #ff5500; color: white;', JSON.parse(overrideText));
+        console.groupCollapsed(`%cMatched XHR Response modified：${matchedInterface.request}`, 'background-color: #108ee9; color: white; padding: 4px');
+        console.info(`%cOriginal Request Url：`, 'background-color: #ff8040; color: white;', this.responseURL);
+        console.info('%cModified Response Payload：', 'background-color: #ff5500; color: white;', JSON.parse(overrideText));
         console.groupEnd();
         // console.info('ⓔ ▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣ ⓔ')
       }
@@ -130,14 +130,18 @@ const ajax_tools_space = {
           const matchedInterface = this._matchedInterface;
           // modify request
           if (matchedInterface) {
-            console.groupCollapsed(`%c Matched XHR Path/Rule：${matchedInterface.request}`, 'background-color: #fa8c16; color: white; padding: 4px');
+            const { replacementUrl, replacementMethod, headers, requestPayloadText } = matchedInterface;
+            if (replacementUrl || replacementMethod || headers || requestPayloadText) {
+              console.groupCollapsed(`%cMatched XHR Request modified：${matchedInterface.request}`, 'background-color: #fa8c16; color: white; padding: 4px');
+              console.info(`%cOriginal Request Url：`, 'background-color: #ff8040; color: white;', requestUrl);
+            }
             if (matchedInterface.replacementUrl && args[1]) {
               args[1] = matchedInterface.replacementUrl;
-              console.info(`%cReplacement Url：`, 'background-color: #ff8040; color: white;', matchedInterface.replacementUrl);
+              console.info(`%cModified Url：`, 'background-color: #ff8040; color: white;', matchedInterface.replacementUrl);
             }
             if (matchedInterface.replacementMethod && args[0]) {
               args[0] = matchedInterface.replacementMethod;
-              console.info(`%cReplacement Method：`, 'background-color: #ff8040; color: white;', matchedInterface.replacementMethod);
+              console.info(`%cModified Method：`, 'background-color: #ff8040; color: white;', matchedInterface.replacementMethod);
             }
             if (matchedInterface.requestPayloadText && args[0] && args[1] && args[0].toUpperCase() === 'GET') {
               const queryStringParameters = ajax_tools_space.getRequestParams(args[1]);
@@ -146,11 +150,19 @@ const ajax_tools_space = {
                 queryStringParameters
               }
               args[1] = ajax_tools_space.executeStringFunction(matchedInterface.requestPayloadText, data);
-              console.info(`%cReplacement Request Payload, GET：`, 'background-color: #ff8040; color: white;', args[1]);
+              console.info(`%cModified Request Payload, GET：`, 'background-color: #ff8040; color: white;', args[1]);
             }
-            console.groupEnd();
           }
           xhr.open && xhr.open.apply(xhr, args);
+        }
+        continue;
+      } else if (attr === 'setRequestHeader') {
+        this.setRequestHeader = (...args) => {
+          this._headerArgs = this._headerArgs ? Object.assign(this._headerArgs, {[args[0]]: args[1]}) : {[args[0]]: args[1]};
+          const matchedInterface = this._matchedInterface;
+          if (!(matchedInterface && matchedInterface.headers)) { // 没有要拦截修改或添加的header
+            xhr.setRequestHeader && xhr.setRequestHeader.apply(xhr, args);
+          }
         }
         continue;
       } else if (attr === 'send') {
@@ -163,22 +175,17 @@ const ajax_tools_space = {
               Object.keys(headers).forEach((key) => {
                 xhr.setRequestHeader && xhr.setRequestHeader.apply(xhr, [key, headers[key]]);
               })
+              console.info(`%cModified Headers：`, 'background-color: #ff8040; color: white;', overrideHeaders);
             }
-            if (matchedInterface.requestPayloadText) {
+            const [method] = this._openArgs;
+            if (matchedInterface.requestPayloadText && method !== 'GET') { // Not GET
               args[0] = ajax_tools_space.executeStringFunction(matchedInterface.requestPayloadText, args[0]);
+              console.info(`%cModified Request Payload, ${method}：`, 'background-color: #ff8040; color: white;', args[0]);
             }
+            console.groupEnd();
           }
           this._sendArgs = args;
           xhr.send && xhr.send.apply(xhr, args);
-        }
-        continue;
-      } else if (attr === 'setRequestHeader') {
-        this.setRequestHeader = (...args) => {
-          this._headerArgs = this._headerArgs ? Object.assign(this._headerArgs, {[args[0]]: args[1]}) : {[args[0]]: args[1]};
-          const matchedInterface = this._matchedInterface;
-          if (!(matchedInterface && matchedInterface.headers)) { // 没有要拦截修改或添加的header
-            xhr.setRequestHeader && xhr.setRequestHeader.apply(xhr, args);
-          }
         }
         continue;
       }
@@ -222,32 +229,37 @@ const ajax_tools_space = {
     const [requestUrl, data] = args;
     const matchedInterface = ajax_tools_space.getMatchedInterface({thisRequestUrl: requestUrl, thisMethod: data && data.method});
     if (matchedInterface && args) {
-      console.groupCollapsed(`%c Matched XHR Path/Rule：${matchedInterface.request}`, 'background-color: #fa8c16; color: white; padding: 4px');
+      const { replacementUrl, replacementMethod, headers, requestPayloadText } = matchedInterface;
+      if (replacementUrl || replacementMethod || headers || requestPayloadText) {
+        console.groupCollapsed(`%cMatched Fetch Request modified：${matchedInterface.request}`, 'background-color: #fa8c16; color: white; padding: 4px');
+        console.info(`%cOriginal Request Url：`, 'background-color: #ff8040; color: white;', requestUrl);
+      }
       if (matchedInterface.replacementUrl && args[0]) {
         args[0] = matchedInterface.replacementUrl;
-        console.info(`%cReplacement Url：`, 'background-color: #ff8040; color: white;', matchedInterface.replacementUrl);
+        console.info(`%cModified Url：`, 'background-color: #ff8040; color: white;', matchedInterface.replacementUrl);
       }
       if (matchedInterface.replacementMethod && args[1]) {
         args[1].method = matchedInterface.replacementMethod;
-        console.info(`%cReplacement Method：`, 'background-color: #ff8040; color: white;', matchedInterface.replacementMethod);
+        console.info(`%cModified Method：`, 'background-color: #ff8040; color: white;', matchedInterface.replacementMethod);
       }
       if (matchedInterface.headers && args[1]) {
         const overrideHeaders = ajax_tools_space.getOverrideText(matchedInterface.headers, data, true);
         args[1].headers = Object.assign(args[1].headers, overrideHeaders);
-        console.info(`%cReplacement Headers：`, 'background-color: #ff8040; color: white;', overrideHeaders);
+        console.info(`%cModified Headers：`, 'background-color: #ff8040; color: white;', overrideHeaders);
       }
       if (matchedInterface.requestPayloadText && args[0] && args[1]) {
-        if (['GET', 'HEAD'].includes(args[1].method.toUpperCase())) {
+        const {method} = args[1];
+        if (['GET', 'HEAD'].includes(method.toUpperCase())) {
           const queryStringParameters = ajax_tools_space.getRequestParams(args[0]);
           const data = {
             requestUrl: args[0],
             queryStringParameters
           }
           args[0] = ajax_tools_space.executeStringFunction(matchedInterface.requestPayloadText, data);
-          console.info(`%cReplacement Request Payload, GET：`, 'background-color: #ff8040; color: white;', args[0]);
+          console.info(`%cModified Request Payload, GET：`, 'background-color: #ff8040; color: white;', args[0]);
         } else {
           data.body = ajax_tools_space.executeStringFunction(matchedInterface.requestPayloadText, data.body);
-          console.info(`%cReplacement Request Payload, NOT GET|HEAD：`, 'background-color: #ff8040; color: white;', data.body);
+          console.info(`%cModified Request Payload, ${method}：`, 'background-color: #ff8040; color: white;', data.body);
         }
       }
       console.groupEnd();
@@ -267,9 +279,9 @@ const ajax_tools_space = {
         };
         overrideText = ajax_tools_space.getOverrideText(matchedInterface.responseText, funcArgs);
         // console.info('ⓢ ►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►► ⓢ');
-        console.groupCollapsed(`%c Matched Fetch Path/Rule：${matchedInterface.request}`, 'background-color: #108ee9; color: white; padding: 4px');
-        console.info(`%cRequest Url：`, 'background-color: #ff8040; color: white;', response.url);
-        console.info('%cResponse Text：', 'background-color: #ff5500; color: white;', JSON.parse(overrideText));
+        console.groupCollapsed(`%cMatched Fetch Response modified：${matchedInterface.request}`, 'background-color: #108ee9; color: white; padding: 4px');
+        console.info(`%cOriginal Request Url：`, 'background-color: #ff8040; color: white;', response.url);
+        console.info('%cModified Response Payload：', 'background-color: #ff5500; color: white;', JSON.parse(overrideText));
         console.groupEnd();
         // console.info('ⓔ ▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣ ⓔ')
       }
