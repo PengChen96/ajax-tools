c8954a460023db18baadf91c6769f6abaedc82b8
robbieyang <robbie.yang@temu.com>
2024-07-28 22:21:15 +0800

    style: 优化展开交互逻辑

--- html/iframePage/main/App.tsx
+++ html/iframePage/main/App.tsx
@@ -380,99 +380,102 @@ function App() {
                   />
                 </Dropdown>
               </div>
-              <Collapse
-                className="ajax-tools-iframe-collapse"
-                defaultActiveKey={['1']}
-                activeKey={item.collapseActiveKeys}
-                onChange={(keys) => onCollapseChange(index, keys)}
-                style={{ borderRadius: 0 }}
-              >
-                {
-                  interfaceList.map((v, i) => {
-                    return <Panel
-                      key={v.key}
-                      header={
-                        <div onClick={e => e.stopPropagation()}>
-                          <div style={{
-                            display: 'inline-grid',
-                            width: 'calc(100vw - 160px)'
-                          }}>
-                            <Input
-                              value={v.request}
-                              onChange={(e) => onInterfaceListChange(index, i, 'request', e.target.value)}
-                              placeholder="Please enter the matching interface"
-                              size="small"
-                              addonBefore={
-                                <Space.Compact>
-                                  <Select
-                                    value={v.matchType}
-                                    onChange={(value) => onInterfaceListChange(index, i, 'matchType', value)}
-                                  >
-                                    <Select.Option value="normal">Normal</Select.Option>
-                                    <Select.Option value="regex">Regex</Select.Option>
-                                  </Select>
-                                  <Select
-                                    dropdownMatchSelectWidth={false}
-                                    value={v.matchMethod}
-                                    onChange={(value) => onInterfaceListChange(index, i, 'matchMethod', value)}
-                                  >
-                                    <Select.Option value="">*(any)</Select.Option>
-                                    { HTTP_METHOD_MAP.map((method) => <Select.Option key={method} value={method}>{method}</Select.Option>) }
-                                  </Select>
-                                </Space.Compact>
-                              }
-                            />
-                            <Input
-                              value={v.requestDes}
-                              onChange={(e) => onInterfaceListChange(index, i, 'requestDes', e.target.value)}
-                              placeholder="Remark（Editable）"
-                              size="small"
-                              className="ajax-tools-iframe-request-des-input"
-                            />
+              {!fold && 
+              (<>
+                <Collapse
+                  className="ajax-tools-iframe-collapse"
+                  defaultActiveKey={['1']}
+                  activeKey={item.collapseActiveKeys}
+                  onChange={(keys) => onCollapseChange(index, keys)}
+                  style={{ borderRadius: 0 }}
+                >
+                  {
+                    interfaceList.map((v, i) => {
+                      return <Panel
+                        key={v.key}
+                        header={
+                          <div onClick={e => e.stopPropagation()}>
+                            <div style={{
+                              display: 'inline-grid',
+                              width: 'calc(100vw - 160px)'
+                            }}>
+                              <Input
+                                value={v.request}
+                                onChange={(e) => onInterfaceListChange(index, i, 'request', e.target.value)}
+                                placeholder="Please enter the matching interface"
+                                size="small"
+                                addonBefore={
+                                  <Space.Compact>
+                                    <Select
+                                      value={v.matchType}
+                                      onChange={(value) => onInterfaceListChange(index, i, 'matchType', value)}
+                                    >
+                                      <Select.Option value="normal">Normal</Select.Option>
+                                      <Select.Option value="regex">Regex</Select.Option>
+                                    </Select>
+                                    <Select
+                                      dropdownMatchSelectWidth={false}
+                                      value={v.matchMethod}
+                                      onChange={(value) => onInterfaceListChange(index, i, 'matchMethod', value)}
+                                    >
+                                      <Select.Option value="">*(any)</Select.Option>
+                                      { HTTP_METHOD_MAP.map((method) => <Select.Option key={method} value={method}>{method}</Select.Option>) }
+                                    </Select>
+                                  </Space.Compact>
+                                }
+                              />
+                              <Input
+                                value={v.requestDes}
+                                onChange={(e) => onInterfaceListChange(index, i, 'requestDes', e.target.value)}
+                                placeholder="Remark（Editable）"
+                                size="small"
+                                className="ajax-tools-iframe-request-des-input"
+                              />
+                            </div>
                           </div>
+                        }
+                        extra={genExtra(index, i, v)}
+                      >
+                        <div style={{ position: 'relative' }}>
+                          <TextArea
+                            rows={4}
+                            value={v.responseText}
+                            onChange={(e) => onInterfaceListChange(index, i, 'responseText', e.target.value)}
+                            placeholder='Response  e.g. { "status": 200, "response": "OK" }'
+                          />
+                          <FormOutlined
+                            title="Edit"
+                            className="ajax-tools-textarea-edit"
+                            onClick={() => modifyDataModalRef.current.openModal({
+                              groupIndex: index,
+                              interfaceIndex: i,
+                              activeTab: 'Response',
+                              request: v.request,
+                              replacementMethod: v.replacementMethod,
+                              replacementUrl: v.replacementUrl,
+                              replacementStatusCode: v.replacementStatusCode,
+                              headersText: v.headers,
+                              requestPayloadText: v.requestPayloadText,
+                              responseLanguage: v.language,
+                              responseText: v.responseText
+                            })}
+                          />
                         </div>
-                      }
-                      extra={genExtra(index, i, v)}
-                    >
-                      <div style={{ position: 'relative' }}>
-                        <TextArea
-                          rows={4}
-                          value={v.responseText}
-                          onChange={(e) => onInterfaceListChange(index, i, 'responseText', e.target.value)}
-                          placeholder='Response  e.g. { "status": 200, "response": "OK" }'
-                        />
-                        <FormOutlined
-                          title="Edit"
-                          className="ajax-tools-textarea-edit"
-                          onClick={() => modifyDataModalRef.current.openModal({
-                            groupIndex: index,
-                            interfaceIndex: i,
-                            activeTab: 'Response',
-                            request: v.request,
-                            replacementMethod: v.replacementMethod,
-                            replacementUrl: v.replacementUrl,
-                            replacementStatusCode: v.replacementStatusCode,
-                            headersText: v.headers,
-                            requestPayloadText: v.requestPayloadText,
-                            responseLanguage: v.language,
-                            responseText: v.responseText
-                          })}
-                        />
-                      </div>
-                    </Panel>;
-                  })
-                }
-              </Collapse>
-              <div className="ajax-tools-iframe-body-footer">
-                <Button
-                  size="small"
-                  type="primary"
-                  shape="circle"
-                  icon={<PlusOutlined/>}
-                  title="Add Interface"
-                  onClick={() => onInterfaceListAdd(index)}
-                />
-              </div>
+                      </Panel>;
+                    })
+                  }
+                </Collapse>
+                <div className="ajax-tools-iframe-body-footer">
+                  <Button
+                    size="small"
+                    type="primary"
+                    shape="circle"
+                    icon={<PlusOutlined/>}
+                    title="Add Interface"
+                    onClick={() => onInterfaceListAdd(index)}
+                  />
+                </div>
+              </>)}
             </div>;
           })
         }
